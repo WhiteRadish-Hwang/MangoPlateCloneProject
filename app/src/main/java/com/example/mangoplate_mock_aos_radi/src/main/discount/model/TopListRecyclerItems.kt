c67e6d745adccc6fbf3c55ac57aaaac49e8e54c5
SY <hain1218@gmail.com>
2021-02-05 17:12:06 +0900

    Add recycler view item in fragment discount toplist tab

---
+++ app/src/main/java/com/example/mangoplate_mock_aos_radi/src/main/discount/model/TopListRecyclerItems.kt
@@ -0,0 +1,3 @@
+package com.example.mangoplate_mock_aos_radi.src.main.discount.model
+
+data class TopListRecyclerItems(val image: String?, val title: String, val subTitle: String, val uplosdDateAgo: String, val viewCount: String)
