1bb5734e3a3683da7505fa5a2de72841cf0c349e
SY. HWang <hain1218@gmail.com>
2021-03-13 07:23:54 +0900

    temp commit

--- app/src/main/java/com/example/mangoplate_mock_aos_radi/src/main/FirebaseImgtest.kt
+++ app/src/main/java/com/example/mangoplate_mock_aos_radi/src/main/FirebaseImgtest.kt
@@ -71,64 +71,6 @@ class FirebaseImgtest: BaseFragment<FragmentFirebaseTestBinding>(FragmentFirebas
 
     }
 
-    fun onMessageReceived(remoteMessage: RemoteMessage) {
-        var message: String? = ""
-        var title: String? = ""
-
-        // Notifition 항목이 있을때.
-        if (remoteMessage.notification != null) {
-            message = remoteMessage.notification!!.body
-            title = remoteMessage.notification!!.title
-        }
-
-        // Data 항목이 있을때.
-        val data = remoteMessage.data
-        val messageData: String = data["message"]!!
-        val titleData: String = data["title"]!!
-        val nameData: String = data["name"]!!
-
-//        //저는 포그라운드 백그라운드 동일하게 컨트롤하기 위해 Data항목에 푸쉬 Title, Body 모두 넣어서 구현하였습니다.
-//        sendNotification(titleData, messageData, nameData)
-    }
-
-//    private fun sendNotification(title: String, message: String, name: String) {
-//        val intent = Intent(this, MainActivity::class.java)
-//        intent.putExtra("name", name) //push 정보중 name 값을 mainActivity로 넘김
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT)
-//        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-//        val notificationBuilder: NotificationCompat.Builder
-//        val notificationManager = getSystemService<Any>(Context.NOTIFICATION_SERVICE as Context) as NotificationManager?
-//
-//        //SDK26부터 푸쉬에 채널항목에 대한 세팅이 필요하다.
-//        if (Build.VERSION.SDK_INT >= 26) {
-//            val channelId = "test push"
-//            val channelName = "test Push Message"
-//            val channelDescription = "New test Information"
-//            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
-//            channel.description = channelDescription
-//            //각종 채널에 대한 설정
-//            channel.enableLights(true)
-//            channel.lightColor = Color.RED
-//            channel.enableVibration(true)
-//            channel.vibrationPattern = longArrayOf(100, 200, 300)
-//            notificationManager!!.createNotificationChannel(channel)
-//            //channel이 등록된 builder
-//            notificationBuilder = Builder(this, channelId)
-//        } else {
-//            notificationBuilder = Builder(this)
-//        }
-//        notificationBuilder.setSmallIcon(R.drawable.ic_stat_ic_notification)
-//                .setContentTitle(title)
-//                .setStyle(BigTextStyle().bigText(message))
-//                .setAutoCancel(true)
-//                .setSound(defaultSoundUri)
-//                .setContentIntent(pendingIntent)
-//                .setContentText(message)
-//        val localTime: Int = TimeUtil().getuniqTime()
-//        notificationManager!!.notify(localTime /* ID of notification */, notificationBuilder.build())
-//    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
